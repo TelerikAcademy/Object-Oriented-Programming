0a4776ba5d84452302204c8ed5846e22c7e19c34
Marto Vesheff <martin.veshev@gmail.com>
2016-06-21 01:25:22 +0300

    Fixed demos and links

--- Topics/05. OOP-Principles-Part-2/demos/Virtual-Methods/Line.cs
+++ Topics/05. OOP-Principles-Part-2/demos/Virtual-Methods/Line.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 
-public class Line : Figure
+class Line : Figure
 {
     public override void Draw()
     {
         Console.WriteLine("I am a line:");
         Console.WriteLine("-----");
-    }
-
-    public override void Important()
-    {
-        throw new NotImplementedException();
     }
 }
