82b38031b46ae6808bf23cdb82b7652e494dd946
KonstantinSimeonov <kon.simeonov@gmail.com>
2016-12-23 22:45:19 +0200

    Added Generic List live demo

---
+++ Topics/02. Defining-Classes-Part-2/demos/Live demo - Generic List/Demos/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+// the other project should be referenced for this using to work
+using CustomDataStructures;
+
+namespace Demos
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var numbers = new GenericList<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                numbers.Add(i);
+            }
+
+            numbers += 99;
+            numbers -= 1;
+
+            Console.WriteLine(numbers.ToString());
+
+            numbers += new int[] { 20, 30, 40 };
+
+            numbers.RemoveFirst(20);
+
+            Console.WriteLine(numbers.ToString());
+
+            numbers[3] = -3;
+            numbers[7] = -7;
+
+            Console.WriteLine(numbers.ToString());
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
